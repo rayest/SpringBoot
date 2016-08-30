f0bf2108a63d0c20bbb5f7b71440d7de7e0c7bd2
Rayest <yi@rayest.cn>
2016-08-30 18:50:33 +0800

    调整类结构

--- src/main/java/cn/rayest/SimpleApplication.java
+++ src/main/java/cn/rayest/SimpleApplication.java
@@ -1,6 +1,5 @@
 package cn.rayest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +15,6 @@ public class SimpleApplication {
     @RequestMapping("/")
     public String index() {
         return "Hello World";
-    }
-
-    @Value("${user.userName}")
-    private String userName;
-    @Value("${user.password}")
-    private String password;
-    @RequestMapping("/user")
-    public String userInfo(){
-        return "userName: " + userName + ", " + "password: " + password;
     }
 
     public static void main(String[] args) {
